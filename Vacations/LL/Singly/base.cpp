50d606cca123d42293cc8a18faac907d5eae378d
ALVF <arindhimar116@gmail.com>
2025-05-25 23:07:40 +0530

    ll

--- Vacations/LL/Singly/base.cpp
+++ Vacations/LL/Singly/base.cpp
@@ -1,65 +1,119 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+class Node {
+public:
     int data;
     Node *next;
 
-    Node(int data){
+    Node(int data) {
         this->data = data;
-        this->next=nullptr;
+        this->next = nullptr;
     }
 };
 
-void menu(){
-    cout<<"1 - Insert\n";
-    cout<<"2 - Update\n";
-    cout<<"3 - Delete\n";
-    cout<<"4 - Display\n";
-    cout<<"5 - Search\n";
-    cout<<"6 - Exit\n";
+void menu() {
+    cout << "\n1 - Insert\n";
+    cout << "2 - Update\n";
+    cout << "3 - Delete\n";
+    cout << "4 - Display\n";
+    cout << "5 - Search\n";
+    cout << "6 - Exit\n";
 }
 
-void insert(Node *&head){
+void insert(Node *&head) {
     int data;
-    cin>>data;
-    if(head==nullptr){
+    cout << "Enter data to insert: ";
+    cin >> data;
+    if (head == nullptr) {
         head = new Node(data);
-    }
-    else{
+    } else {
         Node* temp2 = new Node(data);
         Node* temp = head;
-        for(temp = head;temp->next!=nullptr;temp=temp->next);
-        temp->next=temp2;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = temp2;
     }
 }
 
-void display(Node *&head){
-    for(Node* temp=head;temp!=nullptr;temp=temp->next){
-        cout<<temp->data<<" ";
+void display(Node *head) {
+    if (head == nullptr) {
+        cout << "List is empty.\n";
+        return;
     }
+    for (Node* temp = head; temp != nullptr; temp = temp->next) {
+        cout << temp->data << " ";
+    }
+    cout << endl;
 }
 
-int main()
-{
+void deleteNode(Node *&head) {
+    if (head == nullptr) {
+        cout << "List is empty. Cannot delete.\n";
+        return;
+    }
+
+    int tempData;
+    cout << "Enter data to delete: ";
+    cin >> tempData;
+
+    if (head->data == tempData) {
+        Node* toDelete = head;
+        head = head->next;
+        delete toDelete;
+        return;
+    }
+
+    Node* prev = head;
+    Node* curr = head->next;
+
+    while (curr != nullptr) {
+        if (curr->data == tempData) {
+            prev->next = curr->next;
+            delete curr;
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    cout << "Data not found in the list.\n";
+}
+
+int main() {
     int opt;
     Node* head = nullptr;
-    do{
+    do {
         menu();
-        cout<<"Select option        ";
-        cin>>opt;
+        cout << "Select option: ";
+        cin >> opt;
 
-        switch(opt){
+        switch (opt) {
             case 1:
-            insert(head);
-            break;
+                insert(head);
+                break;
+            case 3:
+                deleteNode(head);
+                break;
             case 4:
-            display(head);
-            break;
+                display(head);
+                break;
+            case 6:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid option. Please try again.\n";
+                break;
         }
 
-    }while(opt!=6);
+    } while (opt != 6);
+
+    while (head != nullptr) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
 
     return 0;
 }
